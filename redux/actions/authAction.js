cf9baa24792d3310c83eb296291fbb55b64ee31b
Carlos <carlos.craig09@gmail.com>
2023-05-25 07:18:23 -0700

    added auth actions

--- redux/actions/authAction.js
+++ redux/actions/authAction.js
@@ -3,10 +3,13 @@ export const REGISTER_USER_FAIL = "REGISTER_USER_FAIL";
 export const LOGIN_USER_SUCCESS = "LOGIN_USER_SUCCESS";
 export const LOGIN_USER_FAIL = "LOGIN_USER_FAIL";
 
+const BASE_URL = "192.168.0.235:3000";
+
 export const registerUser = (authData) => {
 	const { fullName, email, password } = authData;
 
 	// logic to make a post request to REGISTER the user
+	fetch(`${BASE_URL}/api/users/register`);
 	return async (dispatch) => {
 		dispatch({ type: REGISTER_USER_SUCCESS, payload: 1 });
 	};
